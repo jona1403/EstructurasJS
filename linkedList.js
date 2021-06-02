91ce4732b79e639f6123583def721a268452a6c1
Jonathan Alvarado <alexander.2001.aaf@gmail.com>
2021-06-02 06:07:11 -0600

    Double Linked List

---
+++ linkedList.js
@@ -0,0 +1,49 @@
+class Nodo{
+    constructor(dato){
+        this.dato = dato;
+        this.siguiente = null;
+        this.anterior = null;
+    }
+}
+class Lista{
+    constructor(){
+        this.primero = null;
+        this.ultimo = null;
+        this.tamanio = 0;
+    }
+    
+    insertar(dato){
+        var nuevo = Nodo(dato)
+        if(this.primero == null){
+            this.primero = nuevo;
+            this.ultimo = this.primero;
+            this.tamanio++;
+        }else{
+            this.ultimo.siguiente = nuevo;
+            nuevo.anterior = this.ultmo;
+            this.ultimo = nuevo;
+            this.tamanio++;
+        }
+    }
+    eliminar(dato){
+        if(this.primero.dato == dato){
+            this.primero = this.primero.siguiente;
+            this.primero.anterior = null;
+        }else if(this.ultimo.dato == dato){
+            this.ultimo = this.ultimo.anterior;
+            this.ultimo.siguiente = null;
+        }
+        else{
+            var aux = this.primero;
+            while(aux.siguiente.dato != dato){
+                if(aux.siguiente == nil){
+                    console.log("El valor a eliminar no existe");
+                    return;
+                }
+            }
+            aux.siguiente = aux.siguiente.siguiente;
+            aux.siguiente.anterior = aux
+        }
+        this.tamanio--;
+    }
+}
